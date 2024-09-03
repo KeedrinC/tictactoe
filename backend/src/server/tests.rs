883eed1596fe66026fcea707a7faac62bc9946ac
Keedrin Collins <keedrin@gmail.com>
2024-09-03 03:30:13 -0400

    added state to test setup

--- backend/src/server/tests.rs
+++ backend/src/server/tests.rs
@@ -6,25 +6,23 @@ use crate::{handle_socket, AppState};
 
 fn mock_state() -> AppState {
     let state: AppState = AppState::new();
-    let address: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111);
     state
 }
 
-async fn setup() -> (Sender<Result<Message, axum::Error>>, Receiver<Message>) {
+async fn setup() -> (Sender<Result<Message, axum::Error>>, Receiver<Message>, Arc<Mutex<AppState>>) {
+    let connection = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111);
     // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and `Stream`
     let (socket_write, rx) = futures::channel::mpsc::channel(1024);
     let (tx, socket_read) = futures::channel::mpsc::channel(1024);
-    let connection = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111);
-    tokio::spawn(handle_socket(socket_write, socket_read, connection, Arc::new(Mutex::new(mock_state()))));
-    (tx, rx)
+    let state = Arc::new(Mutex::new(mock_state()));
+    tokio::spawn(handle_socket(socket_write, socket_read, connection, state.clone()));
+    (tx, rx, state)
 }
 
 #[tokio::test]
 async fn test_new_connection() {
-    let (mut tx, mut rx) = setup().await;
-    tx.send(Ok(Message::Text(json!({"Connection": {}}).to_string())))
-        .await
-        .unwrap();
+    let (mut tx, mut rx, state) = setup().await;
+    tx.send(Ok(Message::Text(json!({"Connection": {"nickname": "keedrin"}}).to_string()))).await.unwrap();
     let msg = match rx.next().await.unwrap() {
         Message::Text(msg) => dbg!(msg),
         other => panic!("expected a text message but got {other:?}"),
