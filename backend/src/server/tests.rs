0538dca99b511431182bf0d1b75ee630b839718b
Keedrin Collins <keedrin@gmail.com>
2024-09-04 08:26:08 -0400

    Update tests.rs
    - working multiple new connections test

--- backend/src/server/tests.rs
+++ backend/src/server/tests.rs
@@ -5,37 +5,59 @@ use serde::Deserialize;
 use serde_json::json;
 use crate::{handle_socket, AppState};
 
-fn mock_state() -> AppState {
-    let state: AppState = AppState::new();
-    state
+fn mock_state() -> Arc<Mutex<AppState>> {
+    Arc::new(Mutex::new(AppState::new()))
 }
 
-async fn setup() -> (Sender<Result<Message, axum::Error>>, Receiver<Message>, Arc<Mutex<AppState>>) {
-    let connection = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111);
-    // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and `Stream`
+async fn setup(state: Arc<Mutex<AppState>>, connection: Option<SocketAddr>) -> (Sender<Result<Message, axum::Error>>, Receiver<Message>) {
+    let connection = connection.unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111));
     let (socket_write, rx) = futures::channel::mpsc::channel(1024);
     let (tx, socket_read) = futures::channel::mpsc::channel(1024);
-    let state = Arc::new(Mutex::new(mock_state()));
-    tokio::spawn(handle_socket(socket_write, socket_read, connection, state.clone()));
-    (tx, rx, state)
+    tokio::spawn(handle_socket(socket_write, socket_read, connection, state));
+    (tx, rx)
 }
 
 #[tokio::test]
-async fn test_new_connection() {
-    #[derive(Deserialize)]
+async fn test_multiple_new_connections() {
+    #[derive(Debug, Deserialize)]
     struct Session { nickname: Option<String>, token: String }
-    let (mut tx, mut rx, state) = setup().await;
+
+    let state = mock_state(); // shared state between connections
+    let (mut tx, mut rx)
+        = setup(state.clone(), Some(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1111))).await;
+    let (mut tx2, mut rx2)
+        = setup(state.clone(), Some(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 2222))).await;
+
     tx.send(Ok(Message::Text(json!({"Connection": {"nickname": "keedrin"}}).to_string()))).await.unwrap();
+    tx2.send(Ok(Message::Text(json!({"Connection": {"nickname": "keedrin2"}}).to_string()))).await.unwrap();
+
     let msg = match rx.next().await.unwrap() {
         Message::Text(msg) => msg,
         other => panic!("expected a text message but got {other:?}"),
     };
+    let msg2 = match rx2.next().await.unwrap() {
+        Message::Text(msg) => msg,
+        other => panic!("expected a text message but got {other:?}"),
+    };
+
     let state = state.lock().unwrap();
     let response = serde_json::from_str::<Session>(&msg).unwrap();
-    let record = state.sessions.get(&response.token).unwrap().lock().unwrap();
-    let record = record.clone();
+    let record = state.sessions.get(&response.token);
+    assert!(record.is_some());
+    let record = record.unwrap().lock().unwrap().clone();
     assert_eq!(record.token, response.token);
     assert_eq!(record.nickname, response.nickname);
     assert!(record.nickname.is_some());
     assert_eq!(record.nickname.unwrap(), String::from("keedrin"));
+
+    let response = serde_json::from_str::<Session>(&msg2).unwrap();
+    let record = state.sessions.get(&response.token);
+    assert!(record.is_some());
+    let record = record.unwrap().lock().unwrap().clone();
+    assert_eq!(record.token, response.token);
+    assert_eq!(record.nickname, response.nickname);
+    assert!(record.nickname.is_some());
+    assert_eq!(record.nickname.unwrap(), String::from("keedrin2"));
+
+    assert!(state.sessions.len() == 2);
 }
