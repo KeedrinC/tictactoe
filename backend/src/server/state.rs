40a65ba6dee6499b1b53851acd55ab145f1364bf
Keedrin Collins <keedrin@gmail.com>
2024-11-26 10:27:37 -0500

    updated state.rs
    - turned owned objects in state hashmaps into arc<mutex<t>>
    - turned owned types in function signatures into arc<mutex<t>>
    - better function signatures, less options and results where it makes sense
    - working implementations for each function

--- backend/src/server/state.rs
+++ backend/src/server/state.rs
@@ -6,41 +6,39 @@ use crate::session::Session;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub lobbies: HashMap<String, Lobby>,    // lobbies with currently active users
+    pub lobbies: HashMap<String, Arc<Mutex<Lobby>>>,    // lobbies with currently active users
     pub sessions: HashMap<String, Arc<Mutex<Session>>>, // every connection creates a session object
-    pub session_lobby: HashMap<String, Lobby>, // map sessions to current lobbies for easy lookup
+    pub session_lobby: HashMap<String, Arc<Mutex<Lobby>>>, // map sessions to current lobbies for easy lookup
     pub socket_session: HashMap<SocketAddr, Arc<Mutex<Session>>>,   // map sockets to session for easy lookup
 }
 
 impl AppState {
     pub fn new() -> Self {
-        let lobbies: HashMap<String, Lobby> = HashMap::new();
+        let lobbies: HashMap<String, Arc<Mutex<Lobby>>> = HashMap::new();
         let sessions: HashMap<String, Arc<Mutex<Session>>> = HashMap::new();
-        let session_lobby: HashMap<String, Lobby> = HashMap::new();
+        let session_lobby: HashMap<String, Arc<Mutex<Lobby>>> = HashMap::new();
         let socket_session: HashMap<SocketAddr, Arc<Mutex<Session>>> = HashMap::new();
         AppState { lobbies, sessions, socket_session, session_lobby }
     }
 
-    pub fn new_lobby(&mut self, initiator: &mut Arc<Mutex<Session>>) -> Option<&mut Lobby> {
-        let lobby: Lobby = Lobby::new(initiator.clone());
-        self.lobbies.insert(lobby.code.clone(), lobby.clone());
-        let lobby = self.lobbies.get_mut(&lobby.code);
-        if let Some(lobby) = lobby {
-            let session = &initiator.lock().unwrap().token;
-            Some(self.session_lobby.entry(session.to_string()).and_modify(|l| {
-                l.remove_player(initiator.clone()); // remove them from their previous lobby
-                *l = lobby.clone() // now change it to the new lobby
-            }).or_insert(lobby.clone()))
-        } else { None }
+    pub fn new_lobby(&mut self, player_session: Arc<Mutex<Session>>) -> Arc<Mutex<Lobby>> {
+        let lobby: Lobby = Lobby::new(player_session.clone());
+        let new_lobby = Arc::new(Mutex::new(lobby.clone()));
+        let session_token = player_session.lock().unwrap().token.clone();
+        // now make sure the lobby is in both self.lobbies and self.session_lobby
+        let _ = self.leave_lobby(player_session.clone());
+        self.lobbies.insert(lobby.code, new_lobby.clone());
+        self.session_lobby.insert(session_token, new_lobby.clone());
+        new_lobby
     }
 
-    pub fn new_session(&mut self, socket: SocketAddr, nickname: Option<String>) -> Option<Arc<Mutex<Session>>> {
+    pub fn new_session(&mut self, socket: SocketAddr, nickname: Option<String>) -> Arc<Mutex<Session>> {
         let session: Session = Session::new(socket, nickname);
         let session: Arc<Mutex<Session>> = Arc::new(Mutex::new(session));
-        let token = &session.lock().unwrap().token;
+        let token: String = session.lock().unwrap().token.clone();
         self.sessions.insert(token.clone(), session.clone());
         self.socket_session.insert(socket, session.clone());
-        self.sessions.get_mut(token).cloned()
+        session
     }
 
     pub fn move_session(&mut self, socket: SocketAddr, token: &str) -> Option<Arc<Mutex<Session>>> {
@@ -54,18 +52,37 @@ impl AppState {
         } else { None }
     }
 
-    pub fn join_lobby(&mut self, id: &str, session: &mut Arc<Mutex<Session>>) -> Option<&mut Lobby> {
-        let lobby = self.lobbies.get_mut(id).unwrap();
-        lobby.add_player(session.clone());
-        Some(lobby)
+    // the commented lines do not work
+    pub fn join_lobby(&mut self, lobby_code: &str, player_session: Arc<Mutex<Session>>) -> Result<Arc<Mutex<Lobby>>, ()> {
+        if !self.lobbies.contains_key(lobby_code) { return Err(()); }
+        let session_token = player_session.lock().unwrap().token.clone();
+        // Check if the session is already in a session, then leave it
+        if self.session_lobby.contains_key(&session_token) {
+            let _ = self.leave_lobby(player_session.clone());
+        };
+        let lobby = self.lobbies.get(lobby_code).unwrap().clone();
+        // Now that the user isn't in a session, add them to a session and insert into session_lobby
+        self.session_lobby.insert(session_token, lobby.clone());
+        self.lobbies.entry(lobby_code.to_string()).and_modify(|lobby| {
+            let mut lobby_guard = lobby.lock().unwrap();
+            lobby_guard.add_player(player_session.clone());
+        });
+        Ok(lobby)
     }
-
-    pub fn leave_lobby(&mut self, session: &mut Arc<Mutex<Session>>) -> Option<&mut Lobby> {
-        let session_id = &session.lock().unwrap().token;
-        if let Some(lobby) = self.lobbies.get_mut(session_id) {
-            lobby.remove_player(session.clone());
-            self.session_lobby.remove(session_id);
-            Some(lobby)
-        } else { None }
+    pub fn leave_lobby(&mut self, session: Arc<Mutex<Session>>) -> Result<Option<Arc<Mutex<Lobby>>>, ()> {
+        let session_token = session.lock().unwrap().token.clone();
+        if let Some(lobby) = self.session_lobby.get(&session_token) {
+            if let Ok(mut lobby_guard) = lobby.lock() {
+                if lobby_guard.has_player(session.clone()) {
+                    lobby_guard.remove_player(session.clone());
+                    if lobby_guard.player_count() == 0 {
+                        self.lobbies.remove(&lobby_guard.code);
+                    }
+                }
+                return Ok(None);
+            }
+        }
+        self.session_lobby.remove(&session_token);
+        Err(())
     }
 }
