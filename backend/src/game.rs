0bd71ace0939f0bf37f1f9759f37f33cd04c8fcc
Keedrin Collins <keedrin@gmail.com>
2024-12-16 06:43:44 -0500

    Update game.rs
    - made current_player public
    - move_player player argument doesn't need to be owned
    - updated tests

--- backend/src/game.rs
+++ backend/src/game.rs
@@ -6,7 +6,7 @@ pub enum Player {X, O}
 
 #[derive(Clone, Debug)]
 pub struct Game {
-    current_player: Option<Player>,
+    pub current_player: Option<Player>,
     pub board: [Option<Player>; 9]
 }
 
@@ -34,12 +34,12 @@ impl Game {
             println!("{}", if x != 2 { "\n-----------" } else { "\n" });
         }
     }
-    pub fn move_player(&mut self, player: Player, position: usize) -> Result<(), &str> {
+    pub fn move_player(&mut self, player: &Player, position: usize) -> Result<(), &str> {
         if position > 10 { return Err("Position is out of bounds"); }
         if self.board[position].is_some() {
             return Err("A player is already at this location");
         }
-        if player != *self.current_player.as_ref().unwrap() {
+        if !self.current_player.is_some_and(|x| x.eq(player)) {
             return Err("Cannot move the player. It is not their turn.");
         }
         self.board[position].clone_from(&self.current_player);
@@ -66,19 +66,19 @@ mod tests {
         let mut game: Game = Game::new();
         assert_eq!(game.current_player, Some(Player::X));
         assert!(game.board.iter().all(|x| x.is_none()));
-        let _ = game.move_player(Player::X, 0);
+        let _ = game.move_player(&Player::X, 0);
         assert_eq!(game.board[0], Some(Player::X));
-        let _ = game.move_player(Player::O, 4);
+        let _ = game.move_player(&Player::O, 4);
         assert_eq!(game.board[4], Some(Player::O));
-        let _ = game.move_player(Player::X, 8);
+        let _ = game.move_player(&Player::X, 8);
         assert_eq!(game.board[8], Some(Player::X));
-        let _ = game.move_player(Player::O, 2);
+        let _ = game.move_player(&Player::O, 2);
         assert_eq!(game.board[2], Some(Player::O));
-        let _ = game.move_player(Player::X, 6);
+        let _ = game.move_player(&Player::X, 6);
         assert_eq!(game.board[6], Some(Player::X));
-        let _ = game.move_player(Player::O, 7);
+        let _ = game.move_player(&Player::O, 7);
         assert_eq!(game.board[7], Some(Player::O));
-        let _ = game.move_player(Player::X, 3);
+        let _ = game.move_player(&Player::X, 3);
         assert_eq!(game.board[3], Some(Player::X));
     }
 }
