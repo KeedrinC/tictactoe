14e79519b0c0aa9e0f4358bef882cda622483eed
Keedrin Collins <keedrin@gmail.com>
2025-02-21 05:18:46 -0500

    updated messages module
    - added type and data properties to responses
    - changed tuple structures into regular struct syntax
    - added log statements for each message

--- backend/src/server/messages.rs
+++ backend/src/server/messages.rs
@@ -6,14 +6,15 @@ use serde::Deserialize;
 use crate::state::AppState;
 
 #[derive(Debug, Deserialize, Eq, Hash, PartialEq)]
+#[serde(tag = "type", content = "data")]
 pub enum ClientMessage { // these are messages that should be received by the connected client
-    // if a token is passed we attempt to retrieve a session that currently exists
+    // connection: if a token is passed we attempt to retrieve a session that currently exists
     Connection { nickname: Option<String>, access_token: Option<String> },
-    Nickname { nickname: String },                 // changes the nickname of the current session
+    Nickname { nickname: String },          // changes the nickname of the current session
     CreateLobby,                            // creates a new lobby for the current session
-    JoinLobby { id: String },               // moves the current session to an existing lobby
+    JoinLobby { code: String },             // moves the current session to an existing lobby
     StartGame,
-    Move(usize)                             // move the session to a spot in their game
+    Move { position: usize }                // move the session to a spot in their game
 }
 
 impl ClientMessage {
@@ -21,13 +22,13 @@ impl ClientMessage {
         // when we get a message from a client we pass information about the client to its corresponding function
         let state: Arc<Mutex<AppState>> = state.clone();
         match message {
-            ClientMessage::Connection { nickname, access_token: token }
-                => ClientMessage::new_connection(state, socket, nickname, token),
+            ClientMessage::Connection { nickname, access_token }
+                => ClientMessage::new_connection(state, socket, nickname, access_token),
             ClientMessage::Nickname { nickname } => ClientMessage::change_nickname(state, socket, nickname),
             ClientMessage::CreateLobby => ClientMessage::create_lobby(state, socket),
-            ClientMessage::JoinLobby { id } => ClientMessage::join_lobby(state, socket, id),
+            ClientMessage::JoinLobby { code } => ClientMessage::join_lobby(state, socket, code),
             ClientMessage::StartGame => ClientMessage::start_game(state, socket),
-            ClientMessage::Move(position) => ClientMessage::move_message(state, socket, position),
+            ClientMessage::Move { position } => ClientMessage::move_message(state, socket, position),
         }
     }
 
@@ -43,7 +44,13 @@ impl ClientMessage {
             None => Some(state.new_session(socket, nickname.clone())),
         };
         if let Some(session) = session {
-            Ok(json!(*session))
+            let response = json!({
+                "type": "Session",
+                "data": *session
+            });
+            tracing::info!("new_connection {}", response);
+            tracing::info!("new_connection number of sessions {}", state.sessions.len());
+            Ok(response)
         } else { Err(()) }
     }
 
@@ -52,10 +59,12 @@ impl ClientMessage {
         let session = state.socket_session.get(&socket).ok_or(()).cloned()?;
         let mut session_guard = session.lock().unwrap();
         session_guard.set_nickname(&nickname);
-        Ok(json!({
-            "type": "Nickname",
+        let response = json!({
+            "type": "Session",
             "data": *session_guard
-        }))
+        });
+        tracing::info!("change_nickname {}", response);
+        Ok(response)
     }
 
     fn create_lobby(state: Arc<Mutex<AppState>>, socket: SocketAddr) -> Result<serde_json::Value, ()> {
@@ -63,21 +72,26 @@ impl ClientMessage {
         let session = state.socket_session.get(&socket).ok_or(()).cloned()?;
         let new_lobby = state.new_lobby(session.clone());
         let lobby_guard = new_lobby.lock().unwrap();
-        Ok(json!({
-            "type": "CreateLobby",
+        let response = json!({
+            "type": "Lobby",
             "data": *lobby_guard
-        }))
+        });
+        tracing::info!("create_lobby {}", response);
+        tracing::info!("create_lobby number of lobbies {}", state.lobbies.len());
+        Ok(response)
     }
 
-    fn join_lobby(state: Arc<Mutex<AppState>>, socket: SocketAddr, id: String) -> Result<serde_json::Value, ()> {
+    fn join_lobby(state: Arc<Mutex<AppState>>, socket: SocketAddr, code: String) -> Result<serde_json::Value, ()> {
         let mut state = state.lock().unwrap();
         let session = state.socket_session.get(&socket).ok_or(()).cloned()?;
-        let lobby = state.join_lobby(&id, session.clone())?;
+        let lobby = state.join_lobby(&code, session.clone())?;
         let lobby_guard = lobby.lock().unwrap();
-        Ok(json!({
-            "type": "JoinLobby",
+        let response = json!({
+            "type": "Lobby",
             "data": *lobby_guard
-        }))
+        });
+        tracing::info!("join_lobby {}", response);
+        Ok(response)
     }
     
     fn start_game(state: Arc<Mutex<AppState>>, socket: SocketAddr) -> Result<serde_json::Value, ()>  {
@@ -87,10 +101,12 @@ impl ClientMessage {
         let lobby = state.session_lobby.get(session_token).ok_or(()).cloned()?;
         let mut lobby_guard = lobby.lock().unwrap();
         lobby_guard.start_game(); // start the game attached to the lobby
-        Ok(json!({
+        let response = json!({
             "type": "StartGame",
             "data": *lobby_guard
-        }))
+        });
+        tracing::info!("join_lobby {}", response);
+        Ok(response)
     }
 
     fn move_message(state: Arc<Mutex<AppState>>, socket: SocketAddr, position: usize) -> Result<serde_json::Value, ()> {
@@ -109,10 +125,13 @@ impl ClientMessage {
             if let Some(Some((_, player))) = player {
                 let _ = game.move_player(player, position);
             }
+            let response = json!({
+                "type": "Move",
+                "data": game.board
+            });
+            tracing::info!("move_message {}", response);
+            return Ok(response);
         }
-        Ok(json!({
-            "type": "StartGame",
-            "data": *lobby_guard
-        }))
+        Err(())
     }
 }
